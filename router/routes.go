b6d02af3d46e30eafec3ea4ecf39d8c0404f1eb8
Carlos Silva <silvanotfound@gmail.com>
2024-01-17 22:46:44 -0300

    Implementação das rodas

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,42 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func Initialize() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/task", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Teste success",
+			})
 		})
-	})
-	r.Run(":3000")
+
+		v1.GET("/tasks", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Teste success",
+			})
+		})
+
+		v1.POST("/create", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Teste success",
+			})
+		})
+
+		v1.PUT("/update", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Teste success",
+			})
+		})
+
+		v1.DELETE("/delete", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Teste success",
+			})
+		})
+	}
 }
